0b0124d3b9959c235d1d46eb81bc33606fcb49b9
hlcoder2012 <lutfi@cs-linux-035.unbc.ca>
2012-05-31 15:31:24 -0700

    second commit-a frame is created

--- HPad/src/HPad.java
+++ HPad/src/HPad.java
@@ -11,8 +11,10 @@ public class HPad {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		HPad pad=new HPad();
 
 	}
 
