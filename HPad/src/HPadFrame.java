faa8e47d6691bbd7e7201ad910e83a361995268a
hlcoder2012 <lutfi@cs-linux-007.unbc.ca>
2012-06-05 11:20:37 -0700

    adding private variables

--- HPad/src/HPadFrame.java
+++ HPad/src/HPadFrame.java
@@ -1,8 +1,14 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 
 public class HPadFrame extends JFrame
 {
+	private static final int FRAME_WIDTH = 500;
+	private static final int FRAME_HEIGHT = 500;
+	private final JTextArea text;
+	private JPanel panel;
 	
 	public HPadFrame(){}
 
